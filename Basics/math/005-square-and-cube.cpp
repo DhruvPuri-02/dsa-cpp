ff5a2d56bf513f12a9889e9cdb9f51f65b29b836
DhruvPuri-02 <dhruvpuri002@gmail.com>
2025-12-23 21:59:57 +0530

    basics: add square and cube of a number

---
+++ Basics/math/005-square-and-cube.cpp
@@ -0,0 +1,16 @@
+// Problem  : Square and Cube of a Number
+// Topic    : Basics / Math
+// Approach : Use arithmetic operators to compute square and cube
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "Square : " << n * n << endl;
+    cout << "Cube : " << n * n * n;
+
+    return 0;
+}
