b6e2c939b8be7be1ee9897078bd5bc3fe5868d14
DhruvPuri-02 <dhruvpuri002@gmail.com>
2025-12-27 09:52:04 +0530

    patterns: add alphabet right angled triangle

---
+++ Basics/patterns/008-alphabet-right-angled-triangle.cpp
@@ -0,0 +1,21 @@
+// Pattern  : Alphabet Right Angled Triangle
+// Topic    : Patterns
+// Approach : Print characters from 'A' to ('A' + row index)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        char ch = 'A';
+        for (int j = 1; j <= i; j++) {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
