c5004ecbd1db77d0929b167edb6ff8196ecd71c5
DhruvPuri-02 <dhruvpuri002@gmail.com>
2025-12-28 21:59:55 +0530

    basics: add program to print prime numbers till N

---
+++ Basics/math/012-print-prime-numbers.cpp
@@ -0,0 +1,26 @@
+// Program  : Print all prime numbers till N
+// Topic    : Functions
+// Approach : Check each number for divisibility
+#include <iostream>
+using namespace std;
+
+void printPrimes(int n) {
+    for (int i = 2; i <= n; i++) {
+        bool isPrime = true;
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            cout << i << " ";
+        }
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    printPrimes(n);
+    return 0;
+}
