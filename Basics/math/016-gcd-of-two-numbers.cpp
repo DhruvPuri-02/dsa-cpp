5768ff28dbcf1f7546266a06f988cfd4052ddf4a
DhruvPuri-02 <dhruvpuri002@gmail.com>
2025-12-31 18:33:10 +0530

    basics: add gcd of two numbers using Euclidean algorithm

---
+++ Basics/math/016-gcd-of-two-numbers.cpp
@@ -0,0 +1,21 @@
+// Program  : GCD of Two Numbers
+// Topic    : Basics / Math
+// Approach : Euclidean Algorithm
+
+#include <iostream>
+using namespace std;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int rem = a % b;
+        a = b;
+        b = rem;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b);
+    return 0;
+}
